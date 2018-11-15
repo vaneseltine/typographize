c2a7bacd50ad76256343a92ef7e5a3ef13b1789c
Matthew VanEseltine (IRIS) <matvan@umich.edu>
2018-11-15 11:10:05 -0500

    that was a lot easier to get started than I expected

--- start.py
+++ start.py
@@ -10,30 +10,51 @@ F5 to run debug
 ctrl+1/2/3 to switch back to editor.
 """
 
+from itertools import chain
 from pathlib import Path
 import sys
 
 from PIL import Image
-import numpy
+import numpy as np
 
 
 def image_to_array(img):
-    return numpy.array(img.getdata()).reshape((img.height, img.width))
+    return np.array(img.getdata()).reshape((img.height, img.width))
+
+
+def normalize(image_array):
+    """
+    For the purpose of comparing monochromatic arrays,
+    just bin the values.
+    
+    ..todo :: 
+    
+        In the future, probably want to diff from the average,
+        play with the contrast, etc. etc.
+
+    """
+    bins = np.array([np.average(image_array)])
+    return np.digitize(image_array, bins=bins)
 
 
 def main():
-    test_image_path = Path("./image/o.xbm")
-    test_image = Image.open(test_image_path)
-
-    print(test_image.format, test_image.size, test_image.mode)
-    # print(list(test_image.getdata()))
-
-    # print(dir(test_image))
-    width, height = test_image.size
-
-    print(image_to_array(test_image))
-
-    return 0
+    font_glyph_paths = list(Path("./image/liberation_mono/").glob("*"))
+    sample_input_paths = list(Path("./image/handwritten/").glob("*"))
+    for sample_input_path in list(sample_input_paths):
+        matches = {}
+        for font_glyph_path in font_glyph_paths:
+            font_glyph = Image.open(font_glyph_path)
+            # print(font_glyph_path.relative_to("."), font_glyph, sep="\n")
+            sample = Image.open(sample_input_path)
+            # print(sample_input_path.relative_to("."), sample, sep="\n")
+            if font_glyph.size == sample.size:
+                binary_sample = normalize(image_to_array(sample))
+                binary_glyph = normalize(image_to_array(font_glyph))
+                # print(binary_sample)
+                # print(binary_glyph)
+                diffs = np.equal(binary_glyph, binary_sample)
+                matches[font_glyph_path] = np.sum(diffs)
+        print(sample_input_path, *matches.items(), sep="\n    ")
 
 
 if __name__ == "__main__":
