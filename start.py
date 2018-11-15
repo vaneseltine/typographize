dd91dbea3166284712613ed1da3fbd40a0911995
Matthew VanEseltine (IRIS) <matvan@umich.edu>
2018-11-15 09:57:48 -0500

    implemented numpy

--- start.py
+++ start.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python3
-
-print('hi')
+"""
+notes on vscode...
+{
+    "workbench.colorTheme": "Solarized Dark",
+    "python.formatting.provider": "black",
+    "editor.formatOnSave": true,
+}
+F5 to run debug
+ctrl+1/2/3 to switch back to editor.
+"""
 
 from pathlib import Path
+import sys
 
 from PIL import Image
+import numpy
 
-test_image_path = Path('./image/o.xbm')
-test_image = Image.open(test_image_path)
 
-print(test_image.format, test_image.size, test_image.mode)
-print(list(test_image.getdata()))
+def image_to_array(img):
+    return numpy.array(img.getdata()).reshape((img.height, img.width))
 
-width, height = test_image.size
 
-for x in range(width):
-    for y in range(height):
-        coords = (x, y)
-        # print(coords, f'{test_image.getpixel(coords):03d}')
-        print(f'{test_image.getpixel(coords):03d}', end=' ')
-    print('')
+def main():
+    test_image_path = Path("./image/o.xbm")
+    test_image = Image.open(test_image_path)
+
+    print(test_image.format, test_image.size, test_image.mode)
+    # print(list(test_image.getdata()))
+
+    # print(dir(test_image))
+    width, height = test_image.size
+
+    print(image_to_array(test_image))
+
+    return 0
+
+
+if __name__ == "__main__":
+    status = main()
+    # sys.exit(status)
