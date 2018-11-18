ccf5fa0567800a8c066c5fcf65948705143a2dc0
Matt (makeoutpoint) <vaneseltine@gmail.com>
2018-11-18 17:03:29 -0500

    what a mess

--- typographize/typographize.py
+++ typographize/typographize.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 """
-notes on vscode...
-{
-    "workbench.colorTheme": "Solarized Dark",
-    "python.formatting.provider": "black",
-    "editor.formatOnSave": true,
-}
 F5 to run debug
 ctrl+1/2/3 to switch back to editor.
 """
@@ -26,8 +20,8 @@ FULL_CODES = list(
 )
 ASCII_CODES_ONLY = list(range(32, 127))
 
-CODES = list(i for i in ASCII_CODES_ONLY if chr(i) != " ")
-# CODES = FULL_CODES
+# CODES = list(i for i in ASCII_CODES_ONLY if chr(i) != " ")
+CODES = FULL_CODES
 
 
 def dump_glyphs():
@@ -46,10 +40,10 @@ def dump_glyphs():
 def sloppily_binarify(num=None, char=None, font_cfg=None):
     stringed_input = char or chr(num)
     test_image = Image.new(
-        mode="1", size=(font_cfg["w"], font_cfg["h"]), color=0
+        mode="1", size=(font_cfg["w"], font_cfg["h"]), color=1
     )
     draw = ImageDraw.Draw(test_image)
-    draw.text(xy=(0, 0), text=stringed_input, font=font_cfg["font"], fill=1)
+    draw.text(xy=(0, 0), text=stringed_input, font=font_cfg["font"], fill=0)
     binary_glyph = normalize(test_image)
     return binary_glyph
 
@@ -108,19 +102,7 @@ def load_font_cfg(font):
     # test_image = Image.new(mode="1", size=(9, 16), color=112)
 
 
-def image_to_array(img):
-    img_data = img.getdata()
-    ary = np.array(img_data)
-    try:
-        reshaped_ary = ary.reshape((img.height, img.width))
-    except ValueError:
-        print(ary, ary.shape, ary.size, dir(ary))
-
-        sys.exit()
-    return reshaped_ary
-
-
-def normalize(img):
+def normalize(image_array):
     """
     For the purpose of comparing monochromatic arrays,
     just bin the values.
@@ -131,7 +113,7 @@ def normalize(img):
         play with the contrast, etc. etc.
 
     """
-    image_array = image_to_array(img)
+    # image_array = image_to_array(img)
     bins = np.array([np.average(image_array)])
     result = np.digitize(image_array, bins=bins)
     return result
@@ -144,7 +126,7 @@ def blocky_print(*arrays):
     .. todo:: There must surely be a better way to map str dict onto binary arr
     """
     combined_arr = np.concatenate([*arrays], axis=1)
-    blocks = {0: "█", 1: " "}  # ◦
+    blocks = {0: " ", 1: "█"}  # ◦
     for i in combined_arr:
         for j in i:
             print(blocks[j], end="")
@@ -196,8 +178,9 @@ def create_slice_indices(overall_width, char_width, offset=0):
     return list(range(offset, overall_width, char_width))
 
 
-def blaze_samples_matching_and_everything(font_cfg):  # where are you black
-    sample_input_paths = list(Path("./image/handwritten/").glob("**/*.*"))
+def blaze_samples_matching_and_everything(font_cfg):
+    # sample_input_paths = list(Path("./image/handwritten/").glob("**/*.*"))
+    sample_input_paths = list(Path("./image/xbm/").glob("**/*.*"))
     for sample_input_path in list(sample_input_paths):
         print(sample_input_path)
         sample = Image.open(sample_input_path)
@@ -237,6 +220,107 @@ def run_piece_against_fonts(sample, font_cfg):
     return matches
 
 
+class TypoImage:
+    def __init__(self, filepath):
+        self.full_image = Image.open(filepath)
+        self.data = self.full_image.getdata()
+        self.height = self.full_image.height
+        self.width = self.full_image.width
+        self.full_array = np.array(self.data).reshape(self.height, self.width)
+        self.normalized = normalize(self.full_array)
+        self.parts = None
+
+    def split(self, char_height=16, char_width=8, offset=0):
+        """
+        https://docs.scipy.org/doc/numpy-1.10.4/reference/generated/numpy.split.html#numpy.split
+        Parameters:
+        ary : ndarray
+            Array to be divided into sub-arrays.
+        indices_or_sections : int or 1-D array
+            If indices_or_sections is an integer, N,
+                the array will be divided into N equal arrays along axis.
+                If such a split is not possible, an error is raised.
+            If indices_or_sections is a 1-D array of sorted integers,
+                the entries indicate where along axis the array is split.
+                For example, [2, 3] would, for axis=0, result in
+                    ary[:2]
+                    ary[2:3]
+                    ary[3:]
+            If an index exceeds the dimension of the array along axis,
+            an empty sub-array is returned correspondingly.
+        axis : int, optional
+            The axis along which to split, default is 0.
+        Returns:
+        sub-arrays : list of ndarrays
+            A list of sub-arrays.
+        """
+        y_indices = create_slice_indices(self.height, char_height, offset)
+        split_y = np.array_split(self.normalized, y_indices, axis=0)
+        print(split_y)
+        sys.exit()
+
+        x_indices = create_slice_indices(self.width, char_width, offset)
+        # split_up = np.split(ary, 8, axis=1)
+        # split_indices = list((i + 1) * 8 for i in range(int(overall_width / char_width)))
+        # split_indices = [8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88]
+        # print([8, 16, 24, 32, 40, 48, 56])
+        # print(*((i + 1) * 8 for i in range(int(64 / 8))))
+
+        split_x = np.array_split(self.normalized, x_indices, axis=1)
+        print(split_x)
+        sys.exit()
+
+    def cover_multiple(self, current_length, multiple):
+        return ((current_length - 1) // multiple + 1) * multiple
+
+    def slicer(self, chunk_i, chunk_j):
+        a = self.normalized
+        n = self.cover_multiple(a.shape[0], chunk_i)
+        m = self.cover_multiple(a.shape[1], chunk_j)
+        c = np.empty((n, m))
+        c.fill(np.nan)
+        c[: a.shape[0], : a.shape[1]] = a
+        c = c.reshape(n // chunk_i, chunk_i, m // chunk_j, chunk_j)
+        c = c.transpose(0, 2, 1, 3)
+        return c
+
+    def print(self):
+        """
+        Print out arrays as blocks to the screen, side-by-side.
+
+        .. todo:: There must surely be a better way to map str dict onto binary arr
+        """
+        blocks = {0: "█", 1: " "}  # ◦
+        for i in self.normalized:
+            for j in i:
+                print(blocks[j], end="")
+            print("\n", end="")
+
+
+def blocky_print(*arrays):
+    """
+    Print out arrays as blocks to the screen, side-by-side.
+
+    .. todo:: There must surely be a better way to map str dict onto binary arr
+    """
+    combined_arr = np.concatenate([*arrays], axis=1)
+    blocks = {0: "█", 1: " "}  # ◦
+    for i in combined_arr:
+        for j in i:
+            print(blocks[j], end="")
+        print("\n", end="")
+
+
+def image_to_array(img):
+    ary = np.array(self.data)
+    try:
+        reshaped_ary = ary.reshape((img.height, img.width))
+    except ValueError:
+        print(ary, ary.shape, ary.size, dir(ary))
+        sys.exit()
+    return reshaped_ary
+
+
 def main():
     """
 
@@ -252,34 +336,48 @@ def main():
     font = get_font("LiberationMono-Regular", 14)
     # font = get_font("consola", 14)
     font_cfg = load_font_cfg(font)
-    blaze_samples_matching_and_everything(font_cfg)
+    # blaze_samples_matching_and_everything(font_cfg)
 
-    multicol = Image.open(
-        Path("../../downloads/different-blah-74x16.xbm").resolve()
-    )
-    separated_multicol = split_sample(normalize(multicol))
-    winning_chars = []
-    for char_sized_piece in separated_multicol:
-        matches = run_piece_against_fonts(char_sized_piece, font_cfg)
-        if matches:
-            winrars = reversed(sorted(matches.items(), key=lambda x: x[1])[-5:])
-            winrars = list(
-                (
-                    f'{chr(i)} {(score * 1.0 / (font_cfg["w"] * font_cfg["h"])):2.2f}'
-                )
-                for i, score in winrars
-            )
-            print("\n\n", *winrars, sep="    ")
-            best_char = winrars[0][0]
-            winning_chars += [best_char]
-            blocky_print(
-                char_sized_piece,
-                sloppily_binarify(char=best_char, font_cfg=font_cfg),
-            )
-            # blocky_print(char_sized_piece, best_of_the_best)
-    blocky_print(normalize(multicol))
-    print("".join(winning_chars))
+    # multicol = Image.open(
+    #    Path("./image/xbm/different-blah-74x16.xbm").resolve()
+    # )
+    # multicol_img_paths = Path("./image/xbm/").glob("*x35.xbm")
+    multicol_img_paths = Path("./image/xbm/").glob("bigf*.xbm")
+    for img_path in multicol_img_paths:
+
+        print("\n", img_path)
+        multicol = TypoImage(img_path)
+        a = multicol.slicer(16, 8)
+        winning_chars = []
+        for i, row in enumerate(a):
+            for char_sized_piece in row:
+                total_ink = sum(sum(char_sized_piece))
+                if np.isnan(total_ink):
+                    print("Incomplete block.")
+                    continue
+                matches = run_piece_against_fonts(char_sized_piece, font_cfg)
+                if matches:
+                    winrars = reversed(
+                        sorted(matches.items(), key=lambda x: x[1])[-5:]
+                    )
+                    winrars = list(
+                        (
+                            f'{chr(i)} {(score * 1.0 / (font_cfg["w"] * font_cfg["h"])):2.2f}'
+                        )
+                        for i, score in winrars
+                    )
+                    print(*winrars, sep="    ")
+                    best_char = winrars[0][0]
+                    winning_chars += [best_char]
+                    # blocky_print(
+                    #    char_sized_piece,
+                    #    sloppily_binarify(char=best_char, font_cfg=font_cfg),
+                    # )
+            winning_chars += "\n"
+        # multicol.print()
+        print("".join(winning_chars))
 
 
 if __name__ == "__main__":
     status = main()
+
